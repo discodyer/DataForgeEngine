7de4d81586d351c5d1d5954470bbf035655518cb
discodyer <cody23333@gmail.com>
2024-03-13 22:19:03 +0800

    add samples test

--- src/HttpClient.cc
+++ src/HttpClient.cc
@@ -12,24 +12,19 @@ std::string HttpClient::get(const std::string& url) {
     try {
         curlpp::Cleanup cleaner;
         curlpp::Easy request;
-        
         using namespace curlpp::Options;
-
+        std::ostringstream responseStream;
         request.setOpt(new Url(url)); // 设置请求的 URL
-        request.setOpt(new Verbose(false)); // 设置是否显示详细信息
+        // Url url_;
+        // request.getOpt(url_);
+        // std::cout<<"Url:"<<url_.getValue()<<std::endl;
+        request.setOpt<WriteStream>(&responseStream);
         request.perform(); // 执行 HTTP GET 请求
-
-        // 获取响应状态码
-        long responseCode = curlpp::infos::ResponseCode::get(request);
-        std::cout << "Response Code:" << responseCode << std::endl;
-
+        // 获取 HTTP 状态码
+        // long responseCode = curlpp::infos::ResponseCode::get(request);
         // 获取响应内容
-        std::ostringstream responseBody;
-        responseBody << Url("http://example.com");
-        std::cout << "Response body:" << responseBody.str() <<std::endl;
-        
-        return responseBody.str();
-
+        // std::string responseBody = responseStream.str();
+        return responseStream.str();
     } catch (curlpp::RuntimeError &e) {
         return e.what();
     } catch (curlpp::LogicError &e) {
@@ -42,10 +37,11 @@ std::string HttpClient::post(const std::string& url, const std::string& postData
     try {
         std::ostringstream response;
         curlpp::Easy request;
-        request.setOpt<curlpp::options::Url>(url);
-        request.setOpt<curlpp::options::PostFields>(postData);
-        request.setOpt<curlpp::options::PostFieldSize>(postData.length());
-        request.setOpt<curlpp::options::WriteStream>(&response);
+        using namespace curlpp::Options;
+        request.setOpt<Url>(url);
+        request.setOpt<PostFields>(postData);
+        request.setOpt<PostFieldSize>(postData.length());
+        request.setOpt<WriteStream>(&response);
         request.perform();
         return response.str();
     } catch (curlpp::RuntimeError &e) {
