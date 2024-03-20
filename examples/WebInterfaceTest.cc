ad889d31267b4534658f2397f7ac93cb667ab552
discodyer <cody23333@gmail.com>
2024-03-20 22:59:19 +0800

    add Poco Library

--- examples/WebInterfaceTest.cc
+++ examples/WebInterfaceTest.cc
@@ -1,30 +1,7 @@
 #include "WebInterface.h"
 #include <iostream>
 
-// 自定义简单单元测试函数
-void runTest(const std::string& testName, bool condition) {
-    std::cout << "Test \"" << testName << "\": ";
-    if (condition) {
-        std::cout << "PASSED" << std::endl;
-    } else {
-        std::cout << "FAILED" << std::endl;
-    }
-}
-
 int main() {
     // 创建 WebInterface 对象
-    WebInterface webInterface;
-
-    // 准备测试数据
-    std::string url = "http://localhost:8080";
-    std::string postData = "Some data to send";
-
-    // 发送 HTTP POST 请求
-    std::string response = webInterface.sendPostRequest(url, postData);
-
-    // 验证响应是否包含预期内容
-    bool testPassed = response.find("Operation performed successfully") != std::string::npos;
-    runTest("SendPostRequest", testPassed);
-
     return 0;
 }
--- include/DataForge/WebInterface.h
+++ include/DataForge/WebInterface.h
@@ -1,21 +1,9 @@
 #ifndef WEBINTERFACE_H
 #define WEBINTERFACE_H
 
+#include <iostream>
 #include <string>
-#include <curl/curl.h>
 
-class WebInterface {
-public:
-    WebInterface();
 
-    ~WebInterface();
-
-    // 发送 HTTP POST 请求
-    std::string sendPostRequest(const std::string& url, const std::string& postData);
-
-private:
-    // 回调函数用于接收服务器响应数据
-    static size_t writeCallback(void* ptr, size_t size, size_t nmemb, std::string* data);
-};
 
 #endif // WEBINTERFACE_H
